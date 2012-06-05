132feea010d1fc4be2ee76d3f408af41b51f3a84
Janne Ahvo <janne.ahvo@gmail.com>
2012-06-05 11:07:32 +0300

    css includes

---
+++ templates/css_src.template.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="css/reset.css" type="text/css">
+<!-- Less file -->
+<link rel="stylesheet/less" type="text/css" href="css/styles.less">
--- templates/main.tpl.php
+++ templates/main.tpl.php
@@ -1,13 +1,18 @@
 <!doctype html>
 <html>
 	<head>
-		<?PHP include "js_src.template.php" ?>
+		<!-- Stop blocking -->
+		<!--[if IE]><![endif]-->
+		
+<?PHP include "css_src.template.php" ?>
+
+<?PHP include "js_src.template.php" ?>
 		
 	</head>
 	
 	
 	<body>
-	
+		<p>Loremipsum odloret mundi</p>
 	</body>
 
 </html>
