0c3e8dedb9f6c3789e8c2006f77907ac3ce74544
Janne Ahvo <janne.ahvo@gmail.com>
2012-06-06 09:09:51 +0300

    folder iterator

--- api/generator/generator.class.php
+++ api/generator/generator.class.php
@@ -10,11 +10,47 @@ class Generator extends Base{
 	}
 	
 	public function run(){
+	
+		$this->getDirStructure();
+		
 		$main 		= 	$this->get_include_contents("../../templates/main.tpl.php");
 		//$js_src 	= 	get_file_contents("../../templates/js_src.tpl.php");
 		//$content 	= 	get_file_contents("../../templates/content.tpl.php");
 		
 		file_put_contents( "../../index.html", $main );
+	}
+	
+	public function getDirStructure(){
+	
+		$path = realpath('../../');
+		
+		//Directory iterator
+		$dit = new RecursiveDirectoryIterator( $path );
+		// Skip "dot" files 
+		$dit->setFlags(RecursiveDirectoryIterator::SKIP_DOTS);
+		
+		$pit = new ParentIterator($dit);
+		$rit = new RecursiveIteratorIterator( $pit, RecursiveIteratorIterator::SELF_FIRST );
+		
+		foreach ($rit as $fileinfo) {
+			//print_r( $fileinfo );
+			$filename = $fileinfo->getFilename();
+			echo "<br>" . substr ( $filename , 0, 1 )  . "<br>";
+			
+			if( substr ( $filename , 0, 1 ) != "." ){
+				echo "Depth: " . $rit->getDepth();
+    			echo $fileinfo->getPathname() . "<br>";
+    		}
+		}  
+		/*$path = realpath('../../');
+
+		$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST  );
+		
+		foreach($objects as $name => $object){
+    		echo "<br>$name\n";
+    		echo "<br>Depth:" . $objects->getDepth();
+    		echo "Is dir:" . $object->isDir() . "<br>";
+		}*/
 	}
 
 }
