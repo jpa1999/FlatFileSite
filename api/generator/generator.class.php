1f213c176cd3bc0d72424cba9e0f57224cdd182c
Janne Ahvo <janne.ahvo@gmail.com>
2012-06-09 18:56:41 +0300

    find refactored

--- api/generator/generator.class.php
+++ api/generator/generator.class.php
@@ -11,19 +11,21 @@ class Generator extends Base{
 	
 	public function run(){
 	
+		chdir( "../.." );
+		
 		$this->getDirStructure();
 		
-		$main 		= 	$this->get_include_contents("../../templates/main.tpl.php");
+		$main 		= 	$this->get_include_contents("templates/main.tpl.php");
 		//$js_src 	= 	get_file_contents("../../templates/js_src.tpl.php");
 		//$content 	= 	get_file_contents("../../templates/content.tpl.php");
 		
-		file_put_contents( "../../index.html", $main );
+		file_put_contents( "index.html", $main );
 	}
 	
 	public function getDirStructure(){
 		
 		
-		$output = array();
+		$directories = array();
 		//| grep -v '.git
 		//List -Recursively | choose everyline that end to : | 
 		/*$list_recursively 			= "ls -R ../.. ";
@@ -33,21 +35,31 @@ class Generator extends Base{
 		
 		exec( $list_recursively.$grep_folders_only.$remove_path_and_ending.$grep_dot_folders_out, $output );*/
 		
-		exec( "find ../.. -type d -name '[^.]*' -print | sed -e 's/\.\.\/\.\.\///' | grep -v '\.' ", $output );
 		
-		//exec( "find . -type d ! -regex '.*/[.].*'", $output );
+		//exec( "find ../.. -type d -name '[^.]*' -print | sed -e 's/\.\.\/\.\.\///' | grep -v '\.' ", $directories );
+		exec( "find . -type d -regex './[^.].*'", $directories );
 		
 		
-		print_r( $output );
+		print_r( $directories );
+		
+		
+		$this->saveDirectoryList( $directories );
+		
+		
+		
+		
+	}
+	
+	public function saveDirectoryList( $directories ){
 		
 		//Print list out
 		$output_string = "";
 		
 		$level = 0;
 		
-		for( $i=0; $i< count( $output ); $i++ ){
+		for( $i=0; $i< count( $directories ); $i++ ){
 			
-			$components = explode( "/", $output[$i] );
+			$components = explode( "/", $directories[$i] );
 			$previous_level = $level;
 			$level = count( $components );
 			
@@ -55,51 +67,14 @@ class Generator extends Base{
 			if( $level < $previous_level ) $output_string .= "</ul>";
 			
 			$output_string .= "<li>" . array_pop( $components ) . "</li>";
-			
-			//print_r( $components );
-		
-		
+
 		}
 		
 		echo $output_string;
 		
-		//print_r( $output );
-				
-		/*$path = realpath('../../');
-		
-		//Directory iterator
-		$dit = new RecursiveDirectoryIterator( $path );
-		// Skip "dot" files 
-		$dit->setFlags(RecursiveDirectoryIterator::SKIP_DOTS);
-		
-		$pit = new ParentIterator($dit);
-		$rit = new RecursiveIteratorIterator( $pit, RecursiveIteratorIterator::SELF_FIRST );
-		
-		foreach ($rit as $fileinfo) {
-			//print_r( $fileinfo );
-			$filename = $fileinfo->getFilename();
-			echo "<br>" . substr ( $filename , 0, 1 )  . "<br>";
-			
-			if( substr ( $filename , 0, 1 ) != "." ){
-				echo "Depth: " . $rit->getDepth();
-    			echo $fileinfo->getPathname() . "<br>";
-    		}
-		}  
-		
-		// *******************
-		// TEST 3
-		// TEST 4
-		// *******************
-		/*$path = realpath('../../');
-
-		$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST  );
-		
-		foreach($objects as $name => $object){
-    		echo "<br>$name\n";
-    		echo "<br>Depth:" . $objects->getDepth();
-    		echo "Is dir:" . $object->isDir() . "<br>";
-		}*/
+		file_put_contents( "data/structure/index.html", $output_string );
 	}
+	
 
 }
 
